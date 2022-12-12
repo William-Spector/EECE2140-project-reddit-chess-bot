29b90586ca1dce895bcc798fcb1a9cffcb544567
William-Spector <113060480+William-Spector@users.noreply.github.com>
2022-12-11 23:28:21 -0500

    Hit detection, quit from pause menu, reset on song end

--- Level.py
+++ Level.py
@@ -18,18 +18,15 @@ class Level():
         '''
         self.chart = Chart(levelfile)
         self.song = pygame.mixer.Sound(songfile)
+        self.song_len = pygame.mixer.Sound.get_length(self.song) * 1000
+        self.miss = pygame.mixer.Sound("SFX/miss.wav")
+        self.hit = pygame.mixer.Sound("SFX/hit.wav")
+        self.prompt = pygame.mixer.Sound("SFX/prompt.wav")
         self.playing = False
         self.next_note = 0
         self.hits = []
-        self.onscreen_time = 120000/self.chart.bpm  #120,000 ms in 2 minutes, divided by n beats per minute to get ms per 2 beats
-
-    def get_next_note(self):
-        '''
-        function get_next_note
-        Finds the timing for the next note to draw
-        Probably will go unimplemented, unused and be deleted in a future commit
-        '''
-        pass
+        self.onscreen_time = 120000/self.chart.bpm  #120,000 ms is 2 minutes, divided by n beats per minute to get ms per 2 beats
+        self.success = 0
 
     def modify_surface(self, surface, time_mod):
         '''
@@ -37,33 +34,40 @@ class Level():
         inputs:
             surface		    pygame.Surface
             time_mod		timing data for finding current time since 
+        outputs:
+            goto_menu       bool
         '''
         if not self.playing: #if paused, do nothing
             return
         current_note = self.chart.notes[self.next_note] #get the next note to render
         tm = time_mod.get_elapsed()/1000000          #get the current time since level start in ms
-        print(tm) #debugging print statement
+        if current_note != None and tm > (current_note.timing - (self.onscreen_time)): 
+            pygame.mixer.Sound.play(self.prompt)
         while current_note != None and tm > (current_note.timing - (self.onscreen_time)): #while notes need to be printed
             if current_note.color == 0:                 #"green" note, can be any key
-                self.hits.append([50, current_note.location, pygame.Color(0,255,0), current_note.timing])
-                print("green")
+                self.hits.append([50, current_note.location, pygame.Color(0,255,0), current_note.timing, False, 0])
             elif current_note.color == 1:               #"blue" note, button1set
-                self.hits.append([50, current_note.location, pygame.Color(0,0,255), current_note.timing])
-                print("blue")
+                self.hits.append([50, current_note.location, pygame.Color(255,0,0), current_note.timing, False, 1])
             elif current_note.color == 2:               #"red" note, button2set
-                self.hits.append([50, current_note.location, pygame.Color(255,0,0), current_note.timing])
-                print("red")
+                self.hits.append([50, current_note.location, pygame.Color(0,0,255), current_note.timing, False, 2])
             if self.hits[len(self.hits)-1][1][0] == -1: #will be -1, -1 if these are meant to be random
-                self.hits[len(self.hits)-1][1] = (randint(81,1200), randint(81,1200))
+                self.hits[len(self.hits)-1][1] = (randint(81,1200), randint(81,640))
             self.next_note += 1                         #advance next_note, get next_note
             current_note = self.chart.notes[self.next_note]
         surface.fill(black)                             #reset screen for drawing the next frame
+        tm = time_mod.get_elapsed()/1000000             #reset current time before getting animations
         for circle in self.hits:
             circle[0] = 50*((circle[3]-tm)/self.onscreen_time) #calculate proper radius for circle
             pygame.draw.circle(surface,circle[2],circle[1],circle[0]) #draw a circle with correct radius, center, color
-            if(circle[0] <= 0):
+            if(circle[4] and tm > circle[3]):
+                pygame.mixer.Sound.play(self.hit)
+            if(circle[3] < tm - self.chart.leniency * 60000 / self.chart.bpm):
                 self.hits.remove(circle)                # when it's past ideal hit time, stop rendering it
         pygame.display.flip()
+        if(tm > self.song_len):         #if the song is over, return to title, else continue
+            self.next_note = 0
+            return True
+        return False
         
     def pause(self, time_mod):
         '''
@@ -83,14 +87,46 @@ class Level():
             del self.pausetime
         pass
         
-    def check_hit(self, button, time_mod):
+    def check_hit(self, button, time_mod, tm = None, hits = None):
         '''
         Process data to confirm whether or not a button press constitutes a hit
+        prints sucess/failure/no note, timing information
         inputs:
             button      int
             time_mod    timing data
         '''
-        pass
+        if tm == None:
+            tm = time_mod.get_elapsed()/1000000 #setting time is priority number 1
+        if hits == None:
+            hits = self.hits
+        lnc = self.chart.leniency * 60000 / self.chart.bpm #compute leniency in ms
+        if(len(hits) == 0): #If there's no notes left to check for a hit
+            print("none\t", tm, lnc, "fail" "", sep="\t",)
+            pygame.mixer.Sound.play(self.miss)
+            return
+        if(tm > hits[0][3]):        #Hit counts but is after perfect
+            if (hits[0][4]):
+                self.check_hit(button, time_mod, tm, hits[1:])
+            elif(hits[0][5] == 0 or hits[0][5] == button):
+                pygame.mixer.Sound.play(self.hit)
+                print(hits[0][3], tm, lnc, "success", sep="\t",end="")
+                self.success+=1
+            else:
+                self.check_hit(button, time_mod, tm, hits[1:])
+        elif(tm > (hits[0][3] - lnc)): #Hit counts but is before erasure
+            if (hits[0][4]):
+                self.check_hit(button, time_mod, tm, hits[1:])
+            else:
+                if(hits[0][5] == 0 or hits[0][5] == button):
+                    pygame.mixer.Sound.play(self.hit)
+                    print(hits[0][3], tm, lnc, "success", sep="\t",end="")
+                    self.success += 1
+                else:
+                    self.check_hit(button, time_mod, tm, hits[1:])
+        else:   #if it's too far before the current note
+            print(hits[0][3], tm, lnc, "fail",sep="\t",end="")
+            pygame.mixer.Sound.play(self.miss)
+        print("")
        
     def start_level(self, surface, time_mod):
         '''
@@ -102,3 +138,11 @@ class Level():
         pygame.mixer.Sound.play(self.song)  #start music
         time_mod.reset()                    #set initial time
         self.playing = True                 #set playing status
+        
+    def reset(self):
+        '''
+        Resets values that change while playing a level (no inputs/outputs)
+        '''
+        self.next_note = 0
+        self.hits = []
+        self.success = 0
--- main.py
+++ main.py
@@ -30,14 +30,35 @@ levels = [Level("Levels/SpoonsDraft.json", "Music/Spoons-1.wav")] #only one leve
 size = width, height = 1280, 720    #window is 720p for now. Perhaps will find a way to change that if I implement settings, but this is extremely low-priority
 
 screen = pygame.display.set_mode(size)  #set size of the screen
+pygame.display.set_caption('A2BRG')
 black = 0,0,0                           #create color tuple for black
 time_struct = timing()                  #new time struct for timing. Perhaps should be part of the Level class based on other data, but then again it works as it is.
 
-screen.fill(black)                      #for now, start with black screen
-pygame.display.flip()
+#Load and setup as much data as possible
+icon = pygame.image.load("GFX/A2BRG-Icon.png")  #dock/taskbar icon
+title = pygame.image.load("GFX/A2BRG-Title.png")  #title icon for menu
+pygame.Surface.convert_alpha(icon)
+pygame.Surface.convert_alpha(title)
+title_size = title.get_rect().size
+title = pygame.transform.scale(title, (width/3, width/3*title_size[1]/title_size[0]))
+pygame.display.set_icon(icon)
+keyboard_img = pygame.image.load("GFX/keyboard_layout.png")  #keyboard layout graphics for menu
+pygame.Surface.convert_alpha(keyboard_img)
+key_img_size = keyboard_img.get_rect().size
+keyboard_img = pygame.transform.scale(keyboard_img, (width/2, width/2*key_img_size[1]/key_img_size[0]))
+font = pygame.font.Font("Fonts/timeburnerbold.ttf", int(height/20))  #press enter to play font
+proceed_caption = font.render("Press enter to play", False, (0,0,255,255))
+capt_size = proceed_caption.get_rect().size
+pygame.Surface.convert_alpha(proceed_caption)
+
 current_level = 0
 
 while True:                             #begin game loop: start in "menu," meaning wait for return
+    screen.fill(black)
+    screen.blit(title,(width/3,0))
+    screen.blit(keyboard_img,(width/4,height/2))
+    screen.blit(proceed_caption,(width/2-capt_size[0]/2,height-1.5*capt_size[1]))
+    pygame.display.flip()
     while menu:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()    #allow for quit by clicking x
@@ -52,11 +73,19 @@ while True:                             #begin game loop: start in "menu," meani
             if event.type == pygame.KEYDOWN:            #check keys, pause on Esc, check hit on other buttons
                 if event.key == K_ESCAPE:
                     levels[current_level].pause(time_struct)
+                elif not levels[current_level].playing:
+                    if event.key == K_q:                #Quit on escape -> Q
+                        levels[current_level].reset()
+                        menu = True
+                        print('quitting level...')
                 elif event.key in button1set:
-                    time_struct.hit()
-                    levels[current_level].check_hit(1, time_struct)
+                    if levels[current_level].playing:
+                        time_struct.hit()
+                        levels[current_level].check_hit(1, time_struct)
                 elif event.key in button2set:
-                    time_struct.hit()
-                    levels[current_level].check_hit(2, time_struct)
-                
-        levels[0].modify_surface(screen, time_struct) #compute and render next frame
+                    if levels[current_level].playing:
+                        time_struct.hit()
+                        levels[current_level].check_hit(2, time_struct)
+        if not menu:
+            menu = levels[0].modify_surface(screen, time_struct) #compute and render next frame
+    levels[current_level].resset()
